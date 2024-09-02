a39adbefb92a0b46e44c6e2facf57bf0842fc3f4
Amnestia <goldenleon5632@gmail.com>
2024-09-02 11:20:27 +0900

    feat: added nvm to post installation script

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -3,24 +3,29 @@
 #########################
 # Post Installation     #
 #########################
-# Setup AUR Helper
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-
-paru -Syu
-paru -S bash-completion go nomacs 7-zip-full sublime-text-4 fcitx5-mozc docker docker-compose flatpak sway mako waybar wlsunset swaylock greetd swayidle rofi-lbonn-wayland-git tmux tilix python-pipx mkinitcpio-busybox mkinitcpio-firmware fastfetch udisks2 thunar thunderbird-beta-bin wireplumber
 # Setup NTP (https://www.ntppool.org/en/)
 # 0.id.pool.ntp.org 1.id.pool.ntp.org 2.id.pool.ntp.org 3.id.pool.ntp.org
 # 0.jp.pool.ntp.org 1.jp.pool.ntp.org 2.jp.pool.ntp.org 3.jp.pool.ntp.org
-sudo timedatectl set-ntp true
 sudo vim /etc/systemd/timesyncd.conf
-
-sudo systemctl enable systemd-timesyncd.service
+sudo timedatectl set-ntp true
 sudo systemctl enable paccache.timer
 
 # Setup pacman cache config
 paccache -rk1
 paccache -ruk1
 
+# Setup AUR Helper
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+paru -Syu
+paru -S rust bash-completion go nomacs 7-zip-full sublime-text-4 fcitx5-mozc docker docker-compose flatpak sway mako waybar wlsunset swaylock greetd swayidle rofi-lbonn-wayland-git tmux tilix python-pipx fastfetch udisks2 thunar thunderbird-beta-bin wireplumber
 flatpak install flathub org.mozilla.firefox com.usebruno.Bruno io.dbeaver.DBeaverCommunity
+
+source ~/.bashrc
+nvm install --lts
+nvm install-latest-npm
+
+vim +PlugUp
